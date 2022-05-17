9bb40a86f8dc0a1d85f202a1fd9550bf2b65bfc9
Craig Caesar <craig@caesarcreative.com>
2022-05-17 12:34:55 -0500

    broken unit test :( 2

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace SimpleAPI.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value1x", "value2x" };
         }
 
         [HttpGet("{id}")]
